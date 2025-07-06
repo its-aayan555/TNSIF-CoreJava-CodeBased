0cc34d8fba6c02208d215c94dcf0e51af8eecce0
its-aayan555 <aayanmulla7777@gmail.com>
2025-07-06 22:42:24 +0530

    DAY 5.0

---
+++ DAY5.0/src/com/tnsif/day5/associationhasa/Address.java
@@ -0,0 +1,62 @@
+package com.tnsif.day5.associationhasa;
+
+
+public class Address 
+{
+
+	private String street;
+	private String city;
+	private String state;
+	private String postalCode;
+	
+	//	Default constructor
+	public Address() 
+	{
+		super();
+	}
+
+	//	Parameterized constructor
+	public Address(String street, String city, String state, String postalCode) 
+	{
+		super();
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.postalCode = postalCode;
+	}
+
+	//	Getter Setter Method
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
+}
---
+++ DAY5.0/src/com/tnsif/day5/singleinheritance/SingleInheritanceDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.day5.singleinheritance;
+
+
+public class SingleInheritanceDemo 
+{
+
+	
+	public static void main(String[] args) 
+	{
+		
+		Student student = new Student("Aayan", "414319488425","Pune", 1553453215, 1001, "Dr.D Y Patil");
+		System.out.println(student);
+		
+	}
+
+	
+}
